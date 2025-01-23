ba67a92e9e96c962b705447606227f7ed3b8605a
sean roberts <seanroberts66@gmail.com>
2025-01-23 10:10:08 -0800

    remove debug files

--- debug-markdown.ts
+++
@@ -1,7 +0,0 @@
-import { debugMarkdownProcessing } from './lib/debug'
-
-async function main() {
-  await debugMarkdownProcessing('2018-05-23-ohio-data-demo-day.md')
-}
-
-main().catch(console.error) 
